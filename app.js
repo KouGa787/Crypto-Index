3a9e48dd3d9622bf8ca1d9b253b0522d83ff1db5
KouGa <kouga08082361184@gmail.com>
2022-07-21 08:04:43 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const bitcoin = async (fn) => {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=jpy&days=1");
+    const data = await res.json();
+    const elementId = ("btc");
+    fn(data,elementId);
+}
+const ethereum = async (fn) => {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=jpy&days=1");
+    const data = await res.json();
+    const elementId = ("eth");
+    fn(data,elementId);
+}
+
+const dom = (data,elementId) => {
+    const lists = document.getElementById(elementId);
+    const createLists = document.createElement("li");
+    const Value = parseInt(data.prices[0][1]);
+    createLists.innerText = (Value + "円");
+    lists.appendChild(createLists);
+}
+
+bitcoin(dom)
+ethereum(dom)
+
+
+
